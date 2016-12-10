d222d98dc82b6c2795f852e49d9179da9f22ad6b
Mykoladev <Myukolanet@gmail.com>
2016-12-10 14:37:37 +0200

    fix option&add menu

--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
-$(document).ready(function() { 
-$('.slider_top').owlCarousel({
-    center: true,
-    items:2,
-    loop:true,
-    margin:10,
-    responsive:{
-        600:{
-            items:4
-        }
-    }
-});
+// $(document).ready(function() { 
+// $('.slider_top').owlCarousel({
+//     center: true,
+//     items:2,
+//     loop:true,
+//     margin:10,
+//     responsive:{
+//         600:{
+//             items:4
+//         }
+//     }
+// });
 
-});
+// });
